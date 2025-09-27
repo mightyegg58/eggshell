7ae4b94727ed291805e8c7e0f67e70dd27083e49
musk1n <08052006muskan@gmail.com>
2025-09-27 12:23:34 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import './App.css';
-
+import Wallet from "./components/Wallet/Wallet";
+import Navigation from "./components/Navigation/Navigation";
+import DisplayPannel from './components/Display Pannel/DisplayPannel';
 
 function App() {
-return (
-<div className="main-section">
-<h1>Welcome to My Staking App</h1>
-<p>This is the starter version. More features coming soon!</p>
-</div>
-);
+  return (
+    <div className="main-section">
+      <Wallet>
+      <Navigation />
+      <DisplayPannel />
+      </Wallet>
+    </div>
+  );
 }
 
 
