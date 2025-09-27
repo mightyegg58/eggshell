612f5b8e595201296687627f963d049e4ebb0374
“[Harsh <[harshmalik2410@gmail.com]>
2025-09-27 16:14:25 +0530

    added buttons and state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import Wallet from "./components/Wallet/Wallet";
 import Navigation from "./components/Navigation/Navigation";
@@ -6,12 +6,42 @@ import DisplayPannel from './components/Display Pannel/DisplayPannel';
 import { StakingProvider } from './context/StakingContext';
 
 function App() {
+  const [displaySection, setDisplaySection] = useState("stake");
+
+  const handleButtonClick = (section) => {
+    setDisplaySection(section);
+  };
   return (
     <div className="main-section">
       <Wallet>
       <Navigation />
       <StakingProvider>
         <DisplayPannel />
+        <div className="main-content">
+            <div className="button-section">
+              <button
+                onClick={() => handleButtonClick("stake")}
+                className={displaySection === "stake" ? "" : "active"}
+              >
+                Stake
+              </button>
+              <button
+                onClick={() => handleButtonClick("withdraw")}
+                className={displaySection === "withdraw" ? "" : "active"}
+              >
+                Withdraw
+              </button>
+            </div>
+
+            {displaySection === "stake" && (
+              <div className="stake-wrapper">Placeholder for stake UI</div>
+            )}
+
+            {displaySection === "withdraw" && (
+              <div className="stake-wrapper">Placeholder for withdraw UI</div>
+            )}
+          </div>
+
         </StakingProvider>
       </Wallet>
     </div>
