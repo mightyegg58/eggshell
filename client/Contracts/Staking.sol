e6ad5f08f153e1f78fad01701c96d075f3aaad27
AbhinavSandilya <70386713+AbhinavSandilya@users.noreply.github.com>
2025-09-27 15:36:17 +0530

    Update Staking.sol

--- client/Contracts/Staking.sol
+++ client/Contracts/Staking.sol
@@ -29,3 +29,48 @@ contract Staking is ReentrancyGuard{
   event Withdrawn(address indexed user, uint256 indexed amount);
   event RewardsClaimed(address indexed user, uint256 indexed amount);
 
+  constructor(address stakingToken,address rewardToken){
+    // This variable will give us access to IERC20 functions for Staking options
+    s_stakingToken=IERC20(stakingToken);
+    s_rewardToken=IERC20(rewardToken);
+  }
+
+   function rewardPerToken() public view returns(uint){
+    if(totalStakedTokens==0){
+        return rewardPerTokenStored;
+    }
+    uint totalTime = block.timestamp.sub(lastUpdateTime);
+    uint totalRewards = REWARD_RATE.mul(totalTime); 
+    return rewardPerTokenStored.add(totalRewards.mul(1e18).div(totalStakedTokens));
+  }
+
+
+  function earned(address account) public view returns(uint){
+    return stakedBalance[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
+  }
+
+  modifier updateReward(address account){
+    rewardPerTokenStored=rewardPerToken();
+    lastUpdateTime=block.timestamp;
+    rewards[account]=earned(account);
+    userRewardPerTokenPaid[account]=rewardPerTokenStored;
+    _;
+  }
+
+
+    function stake(uint amount) external nonReentrant updateReward(msg.sender){
+    require(amount>0,"Amount must be greater than zero");
+    totalStakedTokens=totalStakedTokens.add(amount);
+    stakedBalance[msg.sender]=stakedBalance[msg.sender].add(amount);
+    emit Staked(msg.sender,amount);
+    bool success = s_stakingToken.transferFrom(msg.sender,address(this),amount);
+    require(success,"Transfer Failed");
+  }
+
+
+  
+
+
+  
+
+}
