c94c3221a02bb0701e8f655e7d2b10eaa72f8540
kawaiibaka0908 <159447814+kawaiibaka0908@users.noreply.github.com>
2025-09-27 15:27:43 +0530

    Create Staking.sol

---
+++ client/Contracts/Staking.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Staking is ReentrancyGuard{
+  using SafeMath for uint256;
+
+  IERC20 public s_stakingToken;
+
+  // This is for Reward token using the ERC20 Interface
+  IERC20 public s_rewardToken;
+
+  uint public constant REWARD_RATE=1e18;
+
+  // We'll keep info of how many people have into our app till now
+  uint private totalStakedTokens;
+  uint public rewardPerTokenStored;
+  uint public lastUpdateTime;
+
+  mapping(address=>uint) public stakedBalance;
+  mapping(address=>uint) public rewards;
+  mapping(address=>uint) public userRewardPerTokenPaid;
+
+  event Staked(address indexed user, uint256 indexed amount);
+  event Withdrawn(address indexed user, uint256 indexed amount);
+  event RewardsClaimed(address indexed user, uint256 indexed amount);
+
